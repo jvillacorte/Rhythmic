f6625792ed21bc9e1625ccf8024cc55da5410309
jvillacorte <joseph.villacorte@gmail.com>
2025-11-06 12:55:58 -0800

    edit

--- Assets/CoinManager.cs
+++ Assets/CoinManager.cs
@@ -8,10 +8,15 @@ public class CoinManager : MonoBehaviour
     public TextMeshProUGUI coinText;
 
     public GameObject door;
+    public GameObject portal;
+    private bool portalActivated;
     private bool doorDestroyed;
     void Start()
     {
-        
+        if (portal != null)
+        {
+            portal.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -19,10 +24,20 @@ public class CoinManager : MonoBehaviour
     {
         coinText.text = "Coin Count: " + coinCount.ToString();
 
-        if(coinCount == 3 && !doorDestroyed)
+        if (coinCount == 3 && !doorDestroyed)
         {
             doorDestroyed = true;
             Destroy(door);
+        }
+        
+        if (coinCount == 5 && !portalActivated)
+        {
+            portalActivated = true;
+            if (portal != null)
+            {
+                portal.SetActive(true);
+            }
+
         }
     }
 }
